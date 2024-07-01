9cdcf81e127f22ca45929150c0893a4ae12ea0b2
Khazulys <53923038+khazulys@users.noreply.github.com>
2024-07-02 03:30:52 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -336,8 +336,5 @@ def main_crack(chat_id, message_id):
 
 if __name__=="__main__":
   os.system('clear')
-  if os.path.exists('usernames.txt'):
-    os.remove(usernames.txt)
-    
   print('bot running!')
   bot.infinity_polling()
