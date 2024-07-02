7d5315929c05bbfbbf935d7fc7fc84cb4c6f9a25
Khazulys <53923038+khazulys@users.noreply.github.com>
2024-07-03 00:15:31 +0700

    update bot.py

--- bot.py
+++ bot.py
@@ -116,7 +116,7 @@ def start_crack(message):
     
     
     #session = requests.Session().max_redirects=100
-    response = requests.get(url, cookies=clean_cookie, headers=headers, allow_redirects=False)
+    response = requests.get(url, cookies=clean_cookie, headers=headers)
     #bot.send_message(chat_id, response.text)
     if response.status_code == 200:
       username = response.json()['user']['username']
